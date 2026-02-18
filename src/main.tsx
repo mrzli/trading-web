023639938eb819b08e72b1f1c7d469f835509e14
Goran Mržljak <goran.mrzljak@gmail.com>
2026-02-18 01:56:51 +0100

    small update

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from '@/app/store';
-import { App } from './App.tsx';
+import { App } from './app.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(

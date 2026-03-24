cfef246f0e1a02b206cfa18f2d4a4123906b641d
Goran Mržljak <goran.mrzljak@gmail.com>
2026-03-24 23:32:00 +0100

    formatting

--- src/app/examples/tailwind-page.tsx
+++ src/app/examples/tailwind-page.tsx
@@ -1,5 +1,7 @@
 import type { FC } from 'react';
 
 export const TailwindPage: FC = () => {
-  return <div className='text-2xl text-blue-500 bg-orange-200'>tailwind-page</div>;
+  return (
+    <div className='bg-orange-200 text-2xl text-blue-500'>tailwind-page</div>
+  );
 };

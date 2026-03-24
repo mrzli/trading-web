3dd26fa3b3126788c75cc8f67ebfa6b60f1224f4
Goran Mržljak <goran.mrzljak@gmail.com>
2026-03-24 22:26:06 +0100

    remvoe a console.log

--- src/app/examples/loader/loader-page.tsx
+++ src/app/examples/loader/loader-page.tsx
@@ -6,8 +6,6 @@ import { loader } from './loader';
 export const LoaderPage: FC = () => {
   const data = useLoaderData<typeof loader>();
 
-  console.log('loader data', data);
-
   return (
     <div>
       <div>loader-page</div>
